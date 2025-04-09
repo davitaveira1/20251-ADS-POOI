c33843c256b2949b81a65b73abcd699ae8ed3107
Davi Taveira <davitaveira@gmail.com>
2025-04-08 22:10:59 -0300

    aula-0804

---
+++ 20251-POOI-producao-Aulas/Aula-0804/src/Ex01.java
@@ -0,0 +1,19 @@
+public class Ex01 {
+
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        double peso;
+
+        nome = "davi";
+        idade = 41;
+        peso = 100.5;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(peso);
+
+    }
+
+
+}
