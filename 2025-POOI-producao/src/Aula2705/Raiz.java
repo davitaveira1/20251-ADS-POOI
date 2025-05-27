b1a0572b0ddc54fe00d423557495542a66ba44f1
Davi Taveira <davitaveira@gmail.com>
2025-05-27 20:16:35 -0300

    aula-2705

---
+++ 2025-POOI-producao/src/Aula2705/Raiz.java
@@ -0,0 +1,11 @@
+package Aula2705;
+
+public class Raiz {
+
+    public static void main(String[] args) {
+        double valor = Math.sqrt(4);
+        System.out.println("Raiz: "+valor);
+    }
+
+
+}
